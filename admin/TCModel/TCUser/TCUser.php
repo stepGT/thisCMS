308ae943e0c3d55dfc0016015d68c3f510c56279
stepGT <stepgt@mail.ru>
2018-12-17 22:35:07 +1000

    Add to class TCUser setter and getter

--- admin/TCModel/TCUser/TCUser.php
+++ admin/TCModel/TCUser/TCUser.php
@@ -11,4 +11,122 @@ namespace Admin\TCModel\TCUser;
 
 class TCUser {
 
+  /**
+   * @var string
+   */
+  protected $table = 'tc_user';
+
+  /**
+   * @var $id
+   */
+  public $id;
+
+  /**
+   * @var $email
+   */
+  public $email;
+
+  /**
+   * @var $password
+   */
+  public $password;
+
+  /**
+   * @var $role
+   */
+  public $role;
+
+  /**
+   * @var $hash
+   */
+  public $hash;
+
+  /**
+   * @var $date_req
+   */
+  public $date_req;
+
+  /**
+   * @return mixed
+   */
+  public function getId() {
+    return $this->id;
+  }
+
+  /**
+   * @param $id
+   */
+  public function setId($id) {
+    $this->id = $id;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getEmail() {
+    return $this->email;
+  }
+
+  /**
+   * @param $email
+   */
+  public function setEmail($email) {
+    $this->email = $email;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getPassword() {
+    return $this->password;
+  }
+
+  /**
+   * @param $password
+   */
+  public function setPassword($password) {
+    $this->password = $password;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getRole() {
+    return $this->role;
+  }
+
+  /**
+   * @param $role
+   */
+  public function setRole($role) {
+    $this->role = $role;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getHash() {
+    return $this->hash;
+  }
+
+  /**
+   * @param $hash
+   */
+  public function setHash($hash) {
+    $this->hash = $hash;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getDateReq() {
+    return $this->date_req;
+  }
+
+  /**
+   * @param $date_req
+   */
+  public function setDateReq($date_req) {
+    $this->date_req = $date_req;
+  }
 }
