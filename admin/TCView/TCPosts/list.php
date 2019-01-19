60c17d6297be2a3d10cd0c6039ccebeb19ab9613
stepGT <stepgt@mail.ru>
2019-01-19 18:27:47 +1000

    Add templates for posts

---
+++ admin/TCView/TCPosts/list.php
@@ -0,0 +1,27 @@
+<?php $this->tcTheme->header(); ?>
+    <main>
+        <div class="container">
+            <h3>Posts <a href="/admin/posts/create/">create</a></h3>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($posts as $post) : ?>
+                    <tr>
+                        <th scope="row"><?= $post->id; ?></th>
+                        <td>
+                            <a href="/admin/posts/edit/<?= $post->id; ?>"><?= $post->title; ?></a>
+                        </td>
+                        <td><?= $post->date; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </main>
+<?php $this->tcTheme->footer(); ?>
