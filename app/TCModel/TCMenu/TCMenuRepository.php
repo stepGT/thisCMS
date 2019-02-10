29e3e724cdb0be66f618e760b4969c8243469167
stepGT <stepgt@mail.ru>
2019-02-10 19:00:37 +1000

    Added menu and menurepository model

--- app/TCModel/TCMenu/TCMenuRepository.php
+++ app/TCModel/TCMenu/TCMenuRepository.php
@@ -7,7 +7,22 @@ use Engine\TCModel;
 
 class TCMenuRepository extends TCModel {
 
-  public function getAllItems() {
+  /**
+   * @param array $params
+   *
+   * @return int
+   */
+  public function add($params = []) {
+    if (empty($params)) {
+      return 0;
+    }
+    $menu = new TCMenu;
+    $menu->setName($params['name']);
+    $menuId = $menu->save();
+    return $menuId;
+  }
+
+  public function getList() {
     $sql = $this->tcQueryBuilder->select()
       ->from('tc_menu')
       ->orderBy('id', 'ASC')
--- app/TCModel/TCMenuItem/TCMenuItemRepository.php
+++ app/TCModel/TCMenuItem/TCMenuItemRepository.php
@@ -24,4 +24,41 @@ class TCMenuItemRepository extends TCModel {
       ->sql();
     return $this->tcDB->tcQuery($sql, $this->tcQueryBuilder->values);
   }
+
+  /**
+   * @param array $params
+   *
+   * @return int
+   */
+  public function add($params = []) {
+    //
+    if (empty($params)) {
+      return 0;
+    }
+    $menuItem = new TCMenuItem;
+    $menuItem->setMenuId($params['menu_id']);
+    $menuItem->setName(self::TC_NEW_MENU_ITEM_NAME);
+    $menuItemId = $menuItem->save();
+    return $menuItemId;
+  }
+
+  /**
+   * @param array $params
+   */
+  public function TCMenuItemRepositorySort($params = []) {
+    $items = isset($params['data']) ? json_decode($params['data']) : [];
+    //
+    if (!empty($items) AND isset($items[0])) {
+      foreach ($items[0] as $position => $item) {
+        $this->tcDB->tcExecute(
+          $this->tcQueryBuilder
+            ->update('tc_menu_item')
+            ->set(['position' => $position])
+            ->where('id', $item->id)
+            ->sql(),
+          $this->tcQueryBuilder->values
+        );
+      }
+    }
+  }
 }
